724fd7c9f56e1fb369dca9bf1883ff08ceef2ea5
Gago <gagmuradyan93@gmail.com>
2019-09-25 17:58:27 +0400

    Single dns details component

---
+++ src/app/components/dns-details/dns-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { DnsService } from 'src/app/services/dns.service';
+import { Observable } from 'rxjs';
+import { Dns } from 'src/app/models/Dns';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-dns-details',
+  templateUrl: './dns-details.component.html',
+  styleUrls: ['./dns-details.component.css'],
+})
+export class DnsDetailsComponent implements OnInit {
+  id: string;
+  notready: boolean;
+  dns$: Observable<Dns>;
+  editing: boolean;
+
+  constructor(private dnsService: DnsService, route: ActivatedRoute) {
+    this.id = route.snapshot.paramMap.get('dnsId');
+    this.editing = false;
+  }
+
+  get loading(): boolean {
+    return this.dnsService.loading.single;
+  }
+
+  ngOnInit() {
+    this.dnsService.loadProvider(this.id);
+    this.dns$ = this.dnsService.provider;
+  }
+
+  switchEditing(): void {
+    this.editing = !this.editing;
+  }
+}
