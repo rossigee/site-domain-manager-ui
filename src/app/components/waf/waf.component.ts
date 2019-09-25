bff38930b85020a6aac0990ff6fd188744aa053d
Gago <gagmuradyan93@gmail.com>
2019-09-25 12:23:03 +0400

    Make simple dropdown with waf entity

---
+++ src/app/components/waf/waf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waf',
+  templateUrl: './waf.component.html',
+  styleUrls: ['./waf.component.css']
+})
+export class WafComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
